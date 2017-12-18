106b6685082f8340053906464f6cb9d66445ac0d
wiesnejt <wiesnejt@mail.uc.edu>
2017-12-17 21:38:14 -0500

    Added serial communication support and different light type support

--- LightShowAC9CHWithLED/LightShowAC9CHWithLED.ino
+++ LightShowAC9CHWithLED/LightShowAC9CHWithLED.ino
@@ -2,24 +2,40 @@
 #include <TimerOne.h>
 
 // Output to Triac pin channels
-unsigned char channel_1 = 3;  
-unsigned char channel_2 = 4;
-unsigned char channel_3 = 5;
-unsigned char channel_4 = 6;
-unsigned char channel_5 = 7;
-unsigned char channel_6 = 8;
-unsigned char channel_7 = 9;
-unsigned char channel_8 = 10;
-unsigned char channel_9 = 11;
+unsigned char channel_1 = 3;  // Layer1Tree
+unsigned char channel_2 = 4;  // Layer2Tree
+unsigned char channel_3 = 5;  // Layer3Tree
+unsigned char channel_4 = 6;  // Layer4Tree
+unsigned char channel_5 = 7;  // SideTree
+unsigned char channel_6 = 8;  // CiramicTree
+unsigned char channel_7 = 9;  // LEDTree
+unsigned char channel_8 = 10; // TopStar
+unsigned char channel_9 = 11; // FireLED
 
+// 0 = Normal, 1 = LED Strand, 2 = LED Star
+unsigned char LightType[] = {0,0,0,0,0,0,1,2,1};
 unsigned char CH1,CH2,CH3,CH4,CH5,CH6,CH7,CH8, CH9;
 unsigned char ChannelNumbers[] = {channel_1, channel_2, channel_3, channel_4, channel_5, channel_6, channel_7, channel_8, channel_9};
 unsigned char CH[]={CH1, CH2, CH3, CH4, CH5, CH6, CH7, CH8, CH9};
 
 unsigned char clock_tick; // variable for Timer1
 
-int incomingByte[2];
-float convert = 255.0/90.0;
+int incomingByte[sizeof(ChannelNumbers)];
+
+int NormalHigh = 4;
+int BulbLEDHigh = 22;
+int StarLEDHigh = 13;
+
+int NormalLow = 90;
+int BulbLEDLow = 95;
+int StarLEDLow = 99;
+
+float convertNormal = 255.0/(float)NormalLow;
+float convertLED = 255.0/(float)BulbLEDLow;
+float convertStar = 255.0/(float)StarLEDLow;
+
+
+
 
 
 void setup() {
@@ -58,44 +74,63 @@ void zero_crosss_int() // function to be fired at the zero crossing to dim the l
 clock_tick=0;
 }
 
-
+int convertNumber(int inByte, int lightType){
+  int invertedValue = 0;
+  int convertedValue = 0;
+  if(lightType == 0){
+    invertedValue = -1*(inByte-255);
+    convertedValue = (int)(invertedValue/convertNormal);
+    if(convertedValue < NormalHigh){
+      convertedValue = NormalHigh;
+    }else if(convertedValue > NormalLow){
+      convertedValue = NormalLow;
+    }
+    return convertedValue;
+  }else if(lightType == 1){
+    invertedValue = -1*(inByte-255);
+    convertedValue = (int)(invertedValue/convertLED);
+    if(convertedValue < BulbLEDHigh){
+      convertedValue = BulbLEDHigh;
+    }else if(convertedValue >BulbLEDLow){
+      convertedValue = BulbLEDLow;
+    }
+    return convertedValue;
+  }else{
+    invertedValue = -1*(inByte-255);
+    convertedValue = (int)(invertedValue/convertStar);
+    if(convertedValue <StarLEDHigh){
+      convertedValue = StarLEDHigh;
+    }else if(convertedValue >StarLEDLow){
+      convertedValue = StarLEDLow;
+    }
+    return convertedValue;
+  }
+}
 
 void loop() {
 
-//  if (Serial.available() >= 2)
-//    {
-//      for (int i=0; i<2;i++)
-//      {
-//        incomingByte[i] = Serial.read();
-//      }                                       // Arduino pins
-//
-//      int r = -1*(incomingByte[0]-255);
-//      int tempint = (int)(r/convert);
-//      if(tempint <4){
-//        tempint = 4;
-//      }else if(tempint >90){
-//        tempint = 90;
-//      }
-//      
-//      CH1 = tempint;
-//    }
+  if (Serial.available() >= sizeof(ChannelNumbers))
+    {
+      for (int i=0; i<sizeof(ChannelNumbers);i++)
+      {
+        incomingByte[i] = Serial.read();
+      }
 
-CH[0] = 4;
-CH[1] = 4;
-CH[2] = 4;
-CH[3] = 4;
-CH[4] = 4;
-CH[5] = 4;
-CH[6] = 22;
-CH[7] = 13;
-CH[8] = 22;
+      for(int c = 0; c < sizeof(CH); c++){
+        int tempInt = convertNumber(incomingByte[c],LightType[c]);
+        CH[c] = tempInt;
+      }
+    }
 
-// Light Type       High  Off
-//  Normal Lights   4     90
-//  Bulb LED        22    85
-//  Star LED        13    92
-
-
+//CH[0] = 4;
+//CH[1] = 4;
+//CH[2] = 4;
+//CH[3] = 4;
+//CH[4] = 4;
+//CH[5] = 4;
+//CH[6] = 22;
+//CH[7] = 13;
+//CH[8] = 22;
 
 
 }
